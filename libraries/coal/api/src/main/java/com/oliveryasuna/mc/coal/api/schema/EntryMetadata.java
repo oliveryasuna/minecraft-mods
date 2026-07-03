df7abfcfb51c2735bc7b87044c870c8b9de49bfe
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2026-07-03 15:48:42 -0400

    #14. Added EntryMetadata.Builder.

--- libraries/coal/api/src/main/java/com/oliveryasuna/mc/coal/api/schema/EntryMetadata.java
+++ libraries/coal/api/src/main/java/com/oliveryasuna/mc/coal/api/schema/EntryMetadata.java
@@ -28,4 +28,30 @@ public interface EntryMetadata {
 
     Optional<String> keyOverride();
 
+    //==================================================
+    // Nested
+    //==================================================
+
+    interface Builder extends org.apache.commons.lang3.builder.Builder<EntryMetadata> {
+
+        //==================================================
+        // Methods
+        //==================================================
+
+        Builder comment(String... lines);
+
+        Builder syncScope(Sync.Scope scope);
+
+        Builder reloadTier(Reload.Tier tier);
+
+        Builder widget(Widget.Type type);
+
+        Builder hidden(boolean hidden);
+
+        Builder addValidator(Validator<?> validator);
+
+        Builder keyOverride(String key);
+
+    }
+
 }
